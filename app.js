79a43fb67eef5bdf8f5a17ecdd3bacc3f5373b9b
TarcisioCosta07 <jctechnologydevcompany@gmail.com>
2025-03-12 19:07:06 +0100

    12.03.25

--- app.js
+++ app.js
@@ -95,3 +95,35 @@ const filterItems = (item) => {
 }
 
 filterTab.forEach(tab => tab.addEventListener ("click", filterItems));
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // Responsive breakpoints
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
